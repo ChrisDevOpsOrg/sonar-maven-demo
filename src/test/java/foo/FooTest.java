50c0bc5303ee52e36db4ddd0a5ab7c77149941b9
Chris <54896350+ChrisDevOpsJourney@users.noreply.github.com>
2023-08-15 09:23:54 +0800

    Update FooTest.java

--- src/test/java/foo/FooTest.java
+++ src/test/java/foo/FooTest.java
@@ -9,4 +9,19 @@ public class FooTest {
   public void testAdd() throws Exception {
   	assertEquals(Foo.div(10, 5), 2);
   }
+
+  @Test
+  public void testDivide() {
+    int a = 56; int b = 10; 
+    double expectedResult = 5.6; 
+    double result = objCalcUnderTest.divide(a, b);
+    Assert.assertEquals(expectedResult, result,0.00005); 
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testDivideByZero() { 
+    int a = 15; int b = 0;
+    objCalcUnderTest.divide(a, b);
+    } 
+  }
 }
