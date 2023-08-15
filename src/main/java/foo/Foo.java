a880ce81bad490235951a6c782ad3a63384ae835
Chris <54896350+ChrisDevOpsJourney@users.noreply.github.com>
2023-08-15 09:54:28 +0800

    Update Foo.java

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -1,15 +1,47 @@
-package foo;
+package calculator;
 
 /**
  * Foo class
  */
-public class Foo {
+public class Calculator {
+  //no-arg constructor
+  public Calculator() {
 
-    public static int div(int a, int b) throws Exception {
-    	if (b == 0) {
-    		throw new UnsupportedOperationException("Can't divide by zero!");
-    	}
-        return a / b;  
+  }
+
+  /**
+  * Sum method.
+  */
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  /**
+  * Subtract method.
+  */
+  public int subtract(int a, int b) {
+    return a - b;
+  }
+
+  /**
+  * Multiply method.
+  */
+  public long multiply(int a, int b) {
+    return a * b;
+  }
+
+  /**
+  * Divide method.
+  * Note that this method throws an exception when
+  * b is zero.
+  */
+  public double divide(int a, int b) {
+    double result;
+    if (b == 0) {
+      throw new IllegalArgumentException("Divisor cannot divide by zero");
+    } else {
+      result = Double.valueOf(a)/Double.valueOf(b);
     }
-
+      return result;
+  }
 }
